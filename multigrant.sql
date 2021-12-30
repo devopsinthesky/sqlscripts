040731f85ea35d4006d0bfc83a4d1c10cb899a96
Marie-Nadin <96870524+devopsinthesky@users.noreply.github.com>
2021-12-30 13:43:27 +0200

    Update multigrant.sql

--- multigrant.sql
+++ multigrant.sql
@@ -4,7 +4,7 @@ DECLARE
 BEGIN
     FOR sch IN SELECT nspname FROM pg_namespace where nspname != 'pg_temp_1' and nspname != 'pg_toast_temp_1'
     LOOP
-        EXECUTE format($$ GRANT USAGE ON SCHEMA %I TO "df-read-only" $$, sch);
+        EXECUTE format($$ GRANT USAGE ON SCHEMA %I TO "user" $$, sch);
         EXECUTE format($$ GRANT SELECT ON ALL TABLES IN SCHEMA %I TO "user" $$, sch);
         EXECUTE format($$ ALTER DEFAULT PRIVILEGES IN SCHEMA %I GRANT SELECT ON TABLES TO "user" $$, sch);
     END LOOP;
